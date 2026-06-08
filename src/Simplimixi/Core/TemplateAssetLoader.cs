7ff7cb6487bbfed6e9e58a30b3920d0dccc407f4
phtuananh21 <phtuananh21@gmail.com>
2026-06-09 01:40:23 +0700

    feat(release): strengthen release security

--- src/Simplimixi/Core/TemplateAssetLoader.cs
+++ src/Simplimixi/Core/TemplateAssetLoader.cs
@@ -8,13 +8,8 @@ namespace CvAut
 {
     internal static class TemplateAssetLoader
     {
-        private static readonly byte[] Magic = { (byte)'S', (byte)'M', (byte)'T', (byte)'P', 1 };
-        private static readonly byte[] Key =
-        {
-            0x53, 0x69, 0x6D, 0x70, 0x6C, 0x69, 0x4D, 0x69,
-            0x78, 0x69, 0x2D, 0x54, 0x65, 0x6D, 0x70, 0x6C,
-            0x61, 0x74, 0x65, 0x73, 0x2D, 0x30, 0x35, 0x31
-        };
+        private static readonly byte[] Magic = { 0x53, 0x4D, 0x54, 0x50, 1 };
+        private static readonly byte[] Key = CreateKey();
 
         public static bool Exists(string templatesRoot, string templateName)
         {
@@ -99,6 +94,31 @@ namespace CvAut
             }
 
             return decoded;
+        }
+
+        private static byte[] CreateKey()
+        {
+            byte[] seed =
+            {
+                0x31, 0xA4, 0x5C, 0x27, 0xE8, 0x09, 0xD3, 0x76,
+                0x42, 0xBD, 0x18, 0xC1, 0x6F, 0x90, 0x2A, 0x55,
+                0xCE, 0x03, 0xB7, 0x64, 0x1D, 0x88, 0xF2, 0x0B,
+                0x79, 0xE1, 0x34, 0xAC, 0x5A, 0x17, 0xC9, 0x60
+            };
+            byte[] mask =
+            {
+                0x4F, 0x12, 0xE0, 0x99, 0x3B, 0xC6, 0x70, 0x2D,
+                0x84, 0x5E, 0xA9, 0x01, 0xF3, 0x6C, 0x1A, 0xD5
+            };
+
+            byte[] key = new byte[24];
+            for (int i = 0; i < key.Length; i++)
+            {
+                int mixed = seed[i] ^ mask[(i * 7 + 3) % mask.Length] ^ (i * 29 + 0x41);
+                key[i] = (byte)((mixed << 3) | (mixed >> 5));
+            }
+
+            return key;
         }
     }
 }
