e9f8de1cd005657d894db0420a48dba9430d18f5
Tuan Anh Pham <41141806+wavycastic@users.noreply.github.com>
2026-07-25 02:43:06 +0700

    refactor(adb): migrate zoom service to helper contract

--- src/backend/Core/ServiceCollectionExtensions.cs
+++ src/backend/Core/ServiceCollectionExtensions.cs
@@ -8,64 +8,60 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddCvAutBackend(this IServiceCollection services, string configPath = "Config/test_config.json")
     {
-        services.AddSingleton<IConfigService>(sp => new ConfigService(configPath));
+        services.AddSingleton<IConfigService>(_ => new ConfigService(configPath));
 
-        services.AddSingleton<IADBHelper>(sp =>
+        services.AddSingleton<IADBHelper>(serviceProvider =>
         {
-            var configService = sp.GetRequiredService<IConfigService>();
-            var devConfig = ConfigManager.GetObjectOrDefault(configService.Config, "device_connection");
-            string host = ConfigManager.GetStringOrDefault(devConfig, "host", "127.0.0.1");
-            int port = ConfigManager.GetIntOrDefault(devConfig, "port", 5556);
+            var configService = serviceProvider.GetRequiredService<IConfigService>();
+            var deviceConfig = ConfigManager.GetObjectOrDefault(configService.Config, "device_connection");
+            string host = ConfigManager.GetStringOrDefault(deviceConfig, "host", "127.0.0.1");
+            int port = ConfigManager.GetIntOrDefault(deviceConfig, "port", 5556);
             return new ADBHelper(host, port);
         });
 
-        services.AddSingleton<IVisionEngine>(sp =>
+        services.AddSingleton<IVisionEngine>(_ =>
         {
             string templatesPath = Path.Combine(AppContext.BaseDirectory, "assets", "Templates");
             return new VisionEngine(templatesPath);
         });
 
-        services.AddSingleton<IPopupHandlerService>(sp =>
+        services.AddSingleton<IPopupHandlerService>(serviceProvider =>
         {
-            var adb = (ADBHelper)sp.GetRequiredService<IADBHelper>();
-            var vision = (VisionEngine)sp.GetRequiredService<IVisionEngine>();
+            var adb = (ADBHelper)serviceProvider.GetRequiredService<IADBHelper>();
+            var vision = (VisionEngine)serviceProvider.GetRequiredService<IVisionEngine>();
             string templatesPath = Path.Combine(AppContext.BaseDirectory, "assets", "Templates");
             return new PopupHandlerService(adb, vision, templatesPath);
         });
 
-        services.AddSingleton<IZoomService>(sp =>
-        {
-            var adb = (ADBHelper)sp.GetRequiredService<IADBHelper>();
-            return new ZoomService(adb);
-        });
+        services.AddSingleton<IZoomService>(serviceProvider =>
+            new ZoomService(serviceProvider.GetRequiredService<IADBHelper>()));
 
-        services.AddSingleton<IStatsRepository>(sp =>
+        services.AddSingleton<IStatsRepository>(serviceProvider =>
         {
-            var adb = (ADBHelper)sp.GetRequiredService<IADBHelper>();
-            var vision = (VisionEngine)sp.GetRequiredService<IVisionEngine>();
+            var adb = (ADBHelper)serviceProvider.GetRequiredService<IADBHelper>();
+            var vision = (VisionEngine)serviceProvider.GetRequiredService<IVisionEngine>();
             string templatesPath = Path.Combine(AppContext.BaseDirectory, "assets", "Templates");
             return new StatsRepository(adb, vision, templatesPath);
         });
 
-        services.AddSingleton<IAccountSwitcher>(sp =>
+        services.AddSingleton<IAccountSwitcher>(serviceProvider =>
         {
-            var adb = (ADBHelper)sp.GetRequiredService<IADBHelper>();
-            var vision = (VisionEngine)sp.GetRequiredService<IVisionEngine>();
+            var adb = (ADBHelper)serviceProvider.GetRequiredService<IADBHelper>();
+            var vision = (VisionEngine)serviceProvider.GetRequiredService<IVisionEngine>();
             string templatesPath = Path.Combine(AppContext.BaseDirectory, "assets", "Templates");
-            return new AccountSwitcher(adb, vision, templatesPath, maxWaitSeconds => true);
+            return new AccountSwitcher(adb, vision, templatesPath, _ => true);
         });
 
-        services.AddSingleton<CVAutomationFramework>(sp =>
+        services.AddSingleton<CVAutomationFramework>(serviceProvider =>
         {
-            var configService = (ConfigService)sp.GetRequiredService<IConfigService>();
-            var adb = (ADBHelper)sp.GetRequiredService<IADBHelper>();
-            var vision = (VisionEngine)sp.GetRequiredService<IVisionEngine>();
+            var configService = (ConfigService)serviceProvider.GetRequiredService<IConfigService>();
+            var adb = (ADBHelper)serviceProvider.GetRequiredService<IADBHelper>();
+            var vision = (VisionEngine)serviceProvider.GetRequiredService<IVisionEngine>();
             string templatesPath = Path.Combine(AppContext.BaseDirectory, "assets", "Templates");
             return new CVAutomationFramework(configPath, configService, adb, vision, templatesPath);
         });
 
         services.AddSingleton<BotOrchestrator>();
-
         return services;
     }
 }
